19695ecb2b69964dbf0ce7064eef4bde590d2ba0
一幻华 <12874587+wx092222@user.noreply.gitee.com>
2025-05-16 20:47:41 +0800

    update

--- astro.config.ts
+++ astro.config.ts
@@ -1,20 +1,18 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import UnoCSS from 'unocss/astro';
+import netlify from '@astrojs/netlify';
 
 export default defineConfig({
   // used to generate images
-  site:
-    process.env.VERCEL_ENV === 'production'
-      ? 'https://brutal.elian.codes/'
-      : process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}/`
-      : 'https://localhost:3000/',
   trailingSlash: 'ignore',
   integrations: [sitemap(), UnoCSS({ injectReset: true })],
+
   vite: {
     optimizeDeps: {
       exclude: ['@resvg/resvg-js'],
     },
   },
+
+  adapter: netlify(),
 });
