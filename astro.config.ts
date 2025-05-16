ca2d9de25fc2d892d226056b8c8f9b2b3f08b34b
一幻华 <12874587+wx092222@user.noreply.gitee.com>
2025-05-16 20:57:40 +0800

    update

--- astro.config.ts
+++ astro.config.ts
@@ -1,17 +1,17 @@
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
-import UnoCSS from 'unocss/astro';
-import netlify from '@astrojs/netlify';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+import UnoCSS from "unocss/astro";
+import netlify from "@astrojs/netlify";
 
 export default defineConfig({
   // used to generate images
-  trailingSlash: 'ignore',
   integrations: [sitemap(), UnoCSS({ injectReset: true })],
   vite: {
     optimizeDeps: {
-      exclude: ['@resvg/resvg-js'],
+      exclude: ["@resvg/resvg-js"],
     },
   },
-  output:'static',
+
+  output: "static",
   adapter: netlify(),
 });
